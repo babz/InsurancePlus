303be34e76ee4d82ea5048de8cc1e7440eac2dd0
Christina <christina123@gmx.at>
2015-04-15 16:06:09 +0200

    hi

--- src/OnlinePortal.java
+++ src/OnlinePortal.java
@@ -1,12 +1,33 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 public class OnlinePortal {
 
-	public static void main() {
-		
-		System.out.println("hello server!");
-		System.out.println("hello babz!");
 
+		
+		public void doGet(HttpServletRequest request, HttpServletResponse response)
+		    throws IOException, ServletException {
+			PrintWriter out = response.getWriter();
+			
+			out.println("hallo");
+
+			out.println("<p>Welcome visitor!</p>");
+			out.println("<p>Please enter your name & password below:</p>");
+			out.println("<p><form action=\"hello\" method=\"post\">");
+			out.println("<p> Name: <input type=\"text\" name=\"name\"><br>");
+			out.println("<p>Password: <input type=\"password\" name=\"password\"><br>");
+			out.println("<p><input type=\"submit\" value=\"Submit\">");
+			out.println("<p></form> ");
+			out.close();
+		}
+		
+		
 
 	}
 
-}
+
